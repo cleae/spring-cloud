65b0814c45186679bd0e76b0ddc5547d4f283ed3
tlin@yeion.com <jyoC80xdSej(em>
2020-11-06 18:10:07 +0800

    api gateway init

---
+++ ec-commerce-api-gateway/src/test/java/com/tl/gateway/eccommerceapigateway/EcCommerceApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tl.gateway.eccommerceapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcCommerceApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
